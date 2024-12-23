d7d8ebcec113a4c75bb197f7dae06a7f2084befc
Abdelrahman <apoeljzzar@gmail.com>
2024-12-23 19:15:41 +0200

    Added User model/Schema

---
+++ src/DB/Models/user.model.js
@@ -0,0 +1,65 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        userName: {
+            type: String,
+            required: [true, "Username is required"],
+            trim : true,
+            minLength: [3, "Username must be at least 3 characters"],
+            maxLength: [20, "Username must be at most 20 characters"],
+        },
+
+        email: {    
+            type: String,
+            required: [true, "Email is required"],
+            unique: [true, "Email must be unique"],
+            trim : true,
+            lowercase: true,
+            match: [
+                /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+            ]
+        }, 
+
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+        },
+
+        gender: {
+            type: String,
+            enum: {
+                values: ["male", "female"],
+                message: "Gender must be either 'male' or 'female'"
+            }
+        },
+
+        confirmEmail: {
+            type: Boolean,
+            default: false
+        },
+
+        role: {
+            type: String,
+            enum: {
+                values: ["user", "admin"],
+                message: "Role must be either 'user' or 'admin'"
+            },
+            default: "user"
+        },
+
+        DOB: String,
+        address: String,
+        phone: String,
+        image: String,
+
+    },
+
+
+    { 
+        timestamps: true 
+    }
+);
+
+const userModel = mongoose.model("User", userSchema);
+export default userModel;
