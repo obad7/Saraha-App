487f3873cfbec3d4c9e5f6ad0f54c8c9ca439dab
Abdelrahman <apoeljzzar@gmail.com>
2024-12-26 16:37:47 +0200

    Encrypt & Decrypt phone Number

--- src/Modules/Auth/auth.service.js
+++ src/Modules/Auth/auth.service.js
@@ -1,9 +1,10 @@
 import userModel from "../../DB/Models/user.model.js";
-import bcrypt from "bcrypt";
+import Bcrypt from "bcrypt";
+import CryptoJS from "crypto-js";
 
 export const register = async (req, res) => {
     try {
-        const { userName, email, password, confirmPassword } = req.body;
+        const { userName, email, password, confirmPassword, phone } = req.body;
 
         if (password !== confirmPassword) {
             return res.status(400).json({ success: false, message: "Passwords do not match" });
@@ -15,12 +16,16 @@ export const register = async (req, res) => {
         }
         
         // hash password
-        const hashPassword = bcrypt.hashSync(password, 10);
+        const hashPassword = Bcrypt.hashSync(password, 10);
+
+        // encrypt phone number        
+        const encryptPhone = CryptoJS.AES.encrypt(phone, process.env.ENCRYPTION_KEY).toString();
 
         const user = await userModel.create({ 
             userName, 
             email, 
-            password : hashPassword 
+            password : hashPassword,
+            phone : encryptPhone,
         });
 
         res.status(201).json({ message: "User registered successfully", user });
@@ -40,10 +45,13 @@ export const login = async (req, res) => {
             return res.status(404).json({ success: false, message: "User does not exist" });
         }
 
-        const match = bcrypt.compareSync(password, user.password);
+        const match = Bcrypt.compareSync(password, user.password);
         if (!match) {
             return res.status(401).json({ success: false, message: "Invalid password" });
         }
+
+        // decrypt phone number
+        user.phone = CryptoJS.AES.decrypt(user.phone, process.env.ENCRYPTION_KEY).toString(CryptoJS.enc.Utf8);
 
         res.status(201).json({ message: "DONE", user });
     } catch (error) {
